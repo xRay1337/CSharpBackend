8df5a10a2c4250f2c80030fd13e92c4fcfb1e5b0
Vitaliy Zlobin <zlovian@yandex.ru>
2020-09-27 16:49:09 +0700

    add  NLog.Config

--- Json/Program.cs
+++ Json/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,23 +13,51 @@ namespace Json
     {
         static void Main(string[] args)
         {
-            var uri = new Uri("https://restcountries.eu/rest/v2/region/americas");
-            var request = WebRequest.Create(uri);
-            var response = request.GetResponse();
-
             string jsonText;
+            var countries = new List<Country>();
+            var logger = LogManager.GetCurrentClassLogger();
 
-            using (var stream = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            logger.Trace("Приложение запущено");
+
+            try
             {
-                jsonText = stream.ReadToEnd();
+                var uri = new Uri("https://restcountries.eu/rest/v2/region/americas");
+                logger.Trace("Подключение к ресурсу: " + uri.ToString());
+                var request = WebRequest.Create(uri);
+                logger.Trace("Ожидание ответа");
+                var response = request.GetResponse();
+
+                logger.Trace("Чтение ответа");
+                using (var stream = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    jsonText = stream.ReadToEnd();
+                }
+
+                logger.Trace("Десериализации ответа");
+                countries = JsonConvert.DeserializeObject<List<Country>>(jsonText);
+            }
+            catch (WebException e)
+            {
+                logger.Error("Ошибка на WEB уровне " + e.Message);
+                throw;
+            }
+            catch (IOException e)
+            {
+                logger.Error("Ошибка на Stream уровне " + e.Message);
+                throw;
+            }
+            catch (Exception e)
+            {
+                logger.Error(e.Message);
+                throw;
             }
 
-            var countries = JsonConvert.DeserializeObject<List<Country>>(jsonText);
-
+            logger.Trace("Подсчёт суммы");
             var sum = countries.Sum(c => c.Population);
 
             Console.WriteLine($"Сумма по странам: {sum}");
 
+            logger.Trace("Подготовка списка валют");
             var allCurrencyName = countries.SelectMany(country => country.Currencies)
                                             .Select(currency => currency.Name)
                                             .Distinct()
@@ -43,6 +72,7 @@ namespace Json
             }
 
             Console.ReadLine();
+            logger.Trace("Приложение остановлено");
         }
     }
 }
